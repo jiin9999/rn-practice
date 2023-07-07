de78a4c613068db02fe5d8df6bf456eb304e7174
jiin9999 <cji3604@gmail.com>
2023-07-07 17:27:25 +0900

    feat: Todo를 저장하고 페인팅하는 기능 추가

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,11 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { theme } from "./colors";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const addTodo = () => {
+    if (text === "") {
+      return;
+    }
+    const newTodos = { ...todos, [Date.now()]: { text, work: working } };
+    setTodos(newTodos);
+    setText("");
+  };
+
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work} activeOpacity={0.5}>
+          <Text
+            style={{
+              ...styles.buttonText,
+              color: working ? "white" : theme.grey,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={travel} ctiveOpacity={0.5}>
+          <Text
+            style={{
+              ...styles.buttonText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        onSubmitEditing={addTodo}
+        onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
+        placeholder={
+          working ? "What do you have to do?" : "Where do you want to go?"
+        }
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(todos).map((key) => (
+          <View style={styles.todo} key={key}>
+            <Text style={styles.todoText}>{todos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +76,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  buttonText: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  todo: {
+    backgroundColor: theme.todoBackground,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  todoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
